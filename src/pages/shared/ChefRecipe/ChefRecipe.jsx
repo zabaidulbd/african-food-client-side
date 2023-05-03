0070c6c5367071d0ba41aa1ee6a9d9034481a240
zabaidulbd <zislam5622@gmail.com>
2023-05-03 09:04:18 +0600

    add recipe details

--- src/pages/shared/ChefRecipe/ChefRecipe.jsx
+++ src/pages/shared/ChefRecipe/ChefRecipe.jsx
@@ -39,6 +39,42 @@ const ChefRecipe = () => {
                     </div>
                 </div>
             </div>
+
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title"><strong>Recipe Name : </strong>  {uniqueRecipe.recipe}</h5>
+                            <p className="card-text"><strong>Ingredients : </strong>  {uniqueRecipe.ingredient}</p>
+                            <p className="card-text"><strong>Cooking Method : </strong>  {uniqueRecipe.method}</p>
+                            <p className="card-text"><strong>Ratings : </strong>{uniqueRecipe.rating}</p>
+                        </div>
+                        <button className='btn btn-primary'>Favorite</button>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title"><strong>Recipe Name : </strong>  {uniqueRecipe.recipe}</h5>
+                            <p className="card-text"><strong>Ingredients : </strong>  {uniqueRecipe.ingredient}</p>
+                            <p className="card-text"><strong>Cooking Method : </strong>  {uniqueRecipe.method}</p>
+                            <p className="card-text"><strong>Ratings : </strong>{uniqueRecipe.rating}</p>
+                        </div>
+                        <button className='btn btn-primary'>Favorite</button>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title"><strong>Recipe Name : </strong>  {uniqueRecipe.recipe}</h5>
+                            <p className="card-text"><strong>Ingredients : </strong>  {uniqueRecipe.ingredient}</p>
+                            <p className="card-text"><strong>Cooking Method : </strong>  {uniqueRecipe.method}</p>
+                            <p className="card-text"><strong>Ratings : </strong>{uniqueRecipe.rating}</p>
+                        </div>
+                        <button className='btn btn-primary'>Favorite</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
