292193aaa58aa4d371baf5e242b0e4b1597f9d47
zabaidulbd <zislam5622@gmail.com>
2023-05-03 23:02:05 +0600

    add loader

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -9,17 +9,21 @@ const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
 
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () => {
+        setLoading(true)
         return signOut(auth)
     }
 
@@ -27,6 +31,7 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, loggedUser => {
             console.log('logged use insite auth state obaserver', loggedUser)
             setUser(loggedUser);
+            setLoading(false)
         })
 
         return () => {
@@ -36,6 +41,7 @@ const AuthProvider = ({ children }) => {
 
     const authInfo = {
         user,
+        loading,
         createUser,
         signIn,
         logOut
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 import { Navigate, useLocation } from 'react-router';
+import { Spinner } from 'react-bootstrap';
 
 const PrivateRoute = ({ children }) => {
-
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
-    console.log(location);
+
+    if (loading) {
+        return <Spinner animation="border" variant="primary" />
+    }
 
     if (user) {
         return children;
