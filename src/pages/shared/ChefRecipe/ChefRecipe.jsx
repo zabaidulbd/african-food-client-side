60fc0912fe42fa644eb8771f56bb8542ef94c82a
zabaidulbd <zislam5622@gmail.com>
2023-05-03 08:36:46 +0600

    add banner in chef details

--- src/pages/shared/ChefRecipe/ChefRecipe.jsx
+++ src/pages/shared/ChefRecipe/ChefRecipe.jsx
@@ -20,13 +20,26 @@ const ChefRecipe = () => {
 
     }, [])
 
-    console.log(uniqueRecipe);
-
 
     return (
-        <div>
-            <h4>from chef recipe</h4>
-        </div>
+        <>
+            <div className="card mb-3">
+                <div className="row g-0">
+                    <div className="col-md-6">
+                        <img src={uniqueRecipe.image} className='img-fluid rounded-start' alt="..." />
+                    </div>
+                    <div className="col-md-6 p-5">
+                        <div className="card-body">
+                            <h2 className="card-title text-center">{uniqueRecipe.name}</h2>
+                            <h5 className="card-text my-5">{uniqueRecipe.description}</h5>
+                            <h6 className="card-text">Total Number of Recipes : {uniqueRecipe.number}</h6>
+                            <h6 className="card-text">Experience :{uniqueRecipe.experience}</h6>
+                            <h6 className="card-text">Likes : {uniqueRecipe.likes}</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
